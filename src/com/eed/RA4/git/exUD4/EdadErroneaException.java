d6f3316646db8ee54f72220e4f150099afff71ee
kiquebarbudo <barbudomolez@gmail.com>
2021-06-07 18:00:59 +0200

    refactor: Miauuuuuu - Enrique Barbudo RECJUN

---
+++ src/com/eed/RA4/git/exUD4/EdadErroneaException.java
@@ -0,0 +1,18 @@
+package com.eed.RA4.git.exUD4;
+	/**
+	 * 
+	 * @author ebarbudo 
+	 *
+	 */
+	public class EdadErroneaException extends Exception {
+	/**
+	 *  Si la edad es menor de 0.
+	 * @param string
+	 */
+		public EdadErroneaException(String string) {
+			super(string);
+		}
+
+	}
+
+
---
+++ src/com/eed/RA4/git/exUD4/Gato.java
@@ -0,0 +1,26 @@
+package com.eed.RA4.git.exUD4;
+
+public class Gato extends Mamifero {
+	private int edad;
+	private String nombre;
+
+	public Gato(String nombre, int edad) throws EdadErroneaException {
+		setAge(edad);
+		this.nombre = nombre;
+	}
+
+	public void setAge(int edad) throws EdadErroneaException {
+		if (edad < 0)
+			throw new EdadErroneaException("La edad no puede ser negativa");
+		this.edad = edad;
+	}
+
+	public int getEdad() {
+		return this.edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+}
