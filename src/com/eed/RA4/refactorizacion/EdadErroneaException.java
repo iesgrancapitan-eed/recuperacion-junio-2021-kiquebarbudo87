699fab869f8539ecfbd4558eac62a3af668b9258
kiquebarbudo <barbudomolez@gmail.com>
2021-06-10 11:02:24 +0200

    refactor: creo clase Hombre para ejemplo

---
+++ src/com/eed/RA4/refactorizacion/EdadErroneaException.java
@@ -0,0 +1,18 @@
+package com.eed.RA4.refactorizacion;
+
+	/**
+	 * Se lanza si la edad es errónea
+	 * @author ebarbudo 
+	 *
+	 */
+	public class EdadErroneaException extends Exception {
+	/**
+	 *  Si la edad es menor de 0.
+	 * @param string
+	 */
+		public EdadErroneaException(String string) {
+			super(string);
+		}
+	}
+
+
---
+++ src/com/eed/RA4/refactorizacion/Hombre.java
@@ -0,0 +1,31 @@
+package com.eed.RA4.refactorizacion;
+
+public class Hombre {
+	private int edad;
+	private String nombre;
+	
+	
+	public Hombre(String nombre, int edad) throws EdadErroneaException {
+		setAge(edad);
+		this.nombre = nombre;
+	}
+
+
+	public void setAge(int edad) throws EdadErroneaException {
+		if (edad < 0)
+			throw new EdadErroneaException("La edad que has introducido no es correcta");
+		this.edad = edad;
+	}
+	
+	public int getEdad() {
+		return this.edad;
+	}
+	
+	public String hablar() {
+		return "Hola, ¿que tal estás? Gracias por darme voz";
+	}
+	public String getNombre() {
+		return nombre;
+	}
+
+}
